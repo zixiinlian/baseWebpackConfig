b0f39549e9456b0e68f821363a4b50a49c261078
hj <hj@l1-qing-yang.hq.ta-mp.com>
2016-02-23 17:49:00 +0800

    add

---
+++ webpack.config.babel.js
@@ -0,0 +1,5 @@
+﻿import devConfig from './webpack.config.dev';
+import productConfig from './webpack.config.product';
+import {argv} from 'yargs';
+let config = argv.dev ? devConfig: productConfig;
+export default config;
